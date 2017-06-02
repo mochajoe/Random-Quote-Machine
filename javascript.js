f80e6aa7483e6ba10e87cf6ff1d5627719246b0e
Anthony Nguyen <anguyee@gmu.edu>
2017-06-02 02:24:25 -0700

    changed the formatting in the div to make it more readible

--- javascript.js
+++ javascript.js
@@ -7,7 +7,11 @@ $(document).ready(function(){
 
 var getQuote = function() {
 $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?", function(json){
-    $('#quotegenerator').text(json.quoteText);
+    var $quoteText = json.quoteText;
+    var $quoteAuthero = json.quoteAuthor;
+    console.log(json);
+    $('#quotegenerator').html(json.quoteText + '<br/><br/><br/><br/><br/>');
+    $('#quotegenerator').append($quoteAuthero);
   })
 }
 
